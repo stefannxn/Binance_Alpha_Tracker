da6f0a7d20dd977a52c39c0d24e00a2acdbc545a
Stefan Chen <140052070+stefannxn@users.noreply.github.com>
2025-05-23 18:41:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -66,3 +66,18 @@ if __name__ == "__main__":
     while True:
         check_new_transfers()
         time.sleep(60)
+
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
+
